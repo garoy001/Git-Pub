29b67fe74877ba8cd154373c7190b0915f8cd869
Gabriel Royce <geroyceus@gmail.com>
2022-11-21 15:19:52 -0500

    002

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const { response } = require('express');
 const express = require('express');
 require('dotenv').config();
+const drinks = require('./models/drinks.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/drinks', (req, res) => {
-	
-})
+	res.send(drinks);
+});
 
 app.listen(port, () => {
 	console.log(`listening to port ${port}`);
