565cac62bf3383252c648a06c9890c1e9cfac5f7
Gabriel Royce <geroyceus@gmail.com>
2022-11-21 16:14:16 -0500

    final

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const drinks = require('./models/drinks.js');
 //pull capitalize func
 const func = require('./functions.js');
+//pull food model
+const food = require('./models/food.js');
 
 // Run the capitalize function on all the drinks names
 for (let element of drinks) {
@@ -15,11 +17,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/drinks', (req, res) => {
-	res.render('drinks_index.ejs', { drinks });
+	res.render('drinks_index.ejs', { drinks, food });
 });
 
 app.get('/drinks/:id', (req, res) => {
-	res.render('drinks_show.ejs', { drinkId: req.params.id });
+	res.render('drinks_show.ejs', { drinks: drinks, drinkId: req.params.id });
+});
+
+app.get('/foods/:id', (req, res) => {
+	res.render('food_show.ejs', { foods: food, foodId: req.params.id });
 });
 
 app.listen(port, () => {
